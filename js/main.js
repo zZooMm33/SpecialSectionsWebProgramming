679d2c60c8a8cdd048ef9566c17bae596207880d
zzoomm <mmmoonnn@mail.ru>
2019-11-25 22:50:26 +0300

    Сделать:  - заголовок (+)  - блоки с пиццами (+)  - кнопки с добавлением в табл (+)  - кнопка перехода в заказ (+)  - input поля для заказа (+)
    Доделать:

--- js/main.js
+++ js/main.js
@@ -7,3 +7,18 @@ function addPizza(button) {
 
     addRowInTable("table-pizza", name, number, price);
 }
+
+function order() {
+    var address = document.getElementById("address").value;
+    var phone_number = document.getElementById("phone_number").value;
+
+    if (address === "") alert("Ошибка ! \nВведите ардрес.");
+    else if (phone_number === "") alert("Ошибка ! \nВведите номер телефона.");
+    else if (!ValidPhone(phone_number)) alert("Ошибка ! \nНомер телефона введен неправильно.");
+}
+
+function ValidPhone(myPhone) {
+    var re = /[\+]\d{1}\s[\(]\d{3}[\)]\s\d{3}[\-]\d{2}[\-]\d{2}/;
+    var valid = re.test(myPhone);
+    return valid;
+}
