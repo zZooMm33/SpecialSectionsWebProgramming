e45481c1d37d88c5370f5837fe3e8eefefad1640
zzoomm <mmmoonnn@mail.ru>
2019-12-14 17:43:45 +0300

    Первый коммит.
    Сделать:
    
    Доделать:

--- js/utils/removeItemInMass.js
+++
@@ -1,6 +0,0 @@
-function remove (arr, indexes) {
-    var arrayOfIndexes = [].slice.call(arguments, 1);  // (1)
-    return arr.filter(function (item, index) {         // (2)
-        return arrayOfIndexes.indexOf(index) == -1;      // (3)
-    });
-}
